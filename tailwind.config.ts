9009f3ca0bee398a180c40a7e830f9391c952be7
YONATANE MEKETE <yonatanemekete33@gmail.com>
2024-09-16 04:28:08 -0700

    setUp the environment

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,26 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      fontFamily: {
+        heading: 'var(--font-heading)',
+        body: 'var(--font-body)',
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        bg: 'hsl(var(--bg))',
+        bglight: 'hsl(var(--bglight))',
+        bglighter: 'hsl(var(--bglighter))',
+        text: 'hsl(var(--text))',
+        textlight: 'hsl(var(--textlight))',
+        textlighter: 'hsl(var(--textlighter))',
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
     },
   },
